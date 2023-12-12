0575c892540a3ecb6267bcc80d340b7df385ac42
K Swamy <swamy.kunta@gmail.com>
2023-12-12 13:30:26 -0500

    Changes

--- src/test/java/com/srtech/controller/CurrencyControllerTest.java
+++ src/test/java/com/srtech/controller/CurrencyControllerTest.java
@@ -3,7 +3,6 @@ package com.srtech.controller;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
