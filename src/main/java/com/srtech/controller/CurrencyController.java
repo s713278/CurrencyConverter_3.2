de02b72851aaeaad6194ac43989c22ff15772940
K Swamy <swamy.kunta@gmail.com>
2023-12-12 18:53:06 -0500

    Logger

--- src/main/java/com/srtech/controller/CurrencyController.java
+++ src/main/java/com/srtech/controller/CurrencyController.java
@@ -1,9 +1,11 @@
 package com.srtech.controller;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.srtech.dto.CurrencyRequest;
 import com.srtech.dto.CurrencyResponse;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/v1")
 public class CurrencyController {
@@ -19,9 +24,22 @@ public class CurrencyController {
 	private double usd_converstion_date=82.5;
 	private double aud_converstion_date=62.5;
 	
+	@GetMapping("/currency/support/currencies")
+	public List<String> getSupportesCurrencies(){
+		log.debug("REST call for getting the supported currencies");
+		
+		/*List<String> list=new ArrayList<>();
+		list.add("INR->USD");
+		list.add("INR->AUD");
+		return list;*/
+		
+		return List.of("INR->USD","INR->AUD");
+	}
+	
+	
 	@PostMapping("/currency/convert")
 	public ResponseEntity<CurrencyResponse> convert(@RequestBody CurrencyRequest currencyRequest) {
-		
+		log.debug("REST call for Currency COnversion and The request is {}",currencyRequest);
 		double totalValue=0;
 		
 		if(currencyRequest.getToCurrency().equals("USD")) {
